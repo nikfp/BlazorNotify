195535d23f9eed7bab7fe7841eff479244bbe442
nikprotsman <nikprotsman@gmail.com>
2021-04-01 11:38:58 -0600

    Add project files.

---
+++ ViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BlazorNotify
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private bool isBusy = false;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                SetValue(ref isBusy, value);
+            }
+        }
+
+        private string myString;
+
+    public string MyString
+    {
+        get { return myString; }
+        set
+        {
+            SetValue<string>(ref myString, value);
+        }
+    }
+
+        private int _counter;
+
+        public int Counter
+        {
+            get { return _counter; }
+            set { SetValue<int>(ref _counter, value); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected void SetValue<T>(
+        ref T backingFiled, 
+        T value, 
+        [CallerMemberName] string propertyName = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(backingFiled, value)) return;
+        backingFiled = value;
+        OnPropertyChanged(propertyName);
+    }
+
+    }
+}
